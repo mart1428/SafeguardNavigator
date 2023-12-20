dfefadf584201da04f2751d6e4db13a648b7381f
ceem <christopherr.martin@mail.utoronto.ca>
2023-12-20 15:54:37 -0500

    conducted initial EDA

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
 def clean_csv_data(filename):
-    df = pd.read_csv(filename)
+   df = pd.read_csv(filename)
 
-    drop_cols = ['X', 'Y', 'OBJECTID', 'REPORT_DATE', \
-       'REPORT_YEAR', 'REPORT_MONTH', 'REPORT_DAY', 'REPORT_DOY', 'REPORT_DOW',\
-       'REPORT_HOUR', 'OCC_YEAR', 'OCC_MONTH', 'OCC_DAY', 'OCC_DOY', 'OCC_DOW',\
-    'DIVISION', 'UCR_CODE',\
-       'UCR_EXT', 'HOOD_158',\
-       'HOOD_140', 'NEIGHBOURHOOD_140']
+   drop_cols = ['X', 'Y', 'OBJECTID', 'REPORT_DATE', \
+      'REPORT_YEAR', 'REPORT_MONTH', 'REPORT_DAY', 'REPORT_DOY', 'REPORT_DOW',\
+      'REPORT_HOUR', 'OCC_YEAR', 'OCC_MONTH', 'OCC_DAY', 'OCC_DOY', 'OCC_DOW',\
+   'DIVISION', 'UCR_CODE',\
+      'UCR_EXT', 'HOOD_158',\
+      'HOOD_140', 'NEIGHBOURHOOD_140']
 
-    df.drop(columns = drop_cols, inplace = True)
-    df = df[(df.LONG_WGS84 != 0) & (df.LAT_WGS84 != 0)]
-    df.to_csv('data_clean.csv', index = False)
+   df.drop(columns = drop_cols, inplace = True)
+   df = df[(df.LONG_WGS84 != 0) & (df.LAT_WGS84 != 0)]
+   date = pd.to_datetime(df['OCC_DATE']).dt.date
+   df['OCC_DATE'] = date
+   df.to_csv('data_clean.csv', index = False)
 
 
 if __name__ == '__main__':
-   pass
-    # clean_csv_data('Major_Crime_Indicators_Open_Data.csv')
+   clean_csv_data('Major_Crime_Indicators_Open_Data.csv')
