4f8cfd8a9810b86da5ffae0c34e7e159c4f77ce5
ceem <christopherr.martin@mail.utoronto.ca>
2023-12-19 22:33:33 -0500

    Added cleaned data as .zip and main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def clean_csv_data(filename):
+    df = pd.read_csv(filename)
+
+    drop_cols = ['X', 'Y', 'OBJECTID', 'REPORT_DATE', \
+       'REPORT_YEAR', 'REPORT_MONTH', 'REPORT_DAY', 'REPORT_DOY', 'REPORT_DOW',\
+       'REPORT_HOUR', 'OCC_YEAR', 'OCC_MONTH', 'OCC_DAY', 'OCC_DOY', 'OCC_DOW',\
+    'DIVISION', 'UCR_CODE',\
+       'UCR_EXT', 'HOOD_158',\
+       'HOOD_140', 'NEIGHBOURHOOD_140']
+
+    df.drop(columns = drop_cols, inplace = True)
+    df = df[(df.LONG_WGS84 != 0) & (df.LAT_WGS84 != 0)]
+    df.to_csv('data_clean.csv', index = False)
+
+
+if __name__ == '__main__':
+   pass
+    # clean_csv_data('Major_Crime_Indicators_Open_Data.csv')
